97671f3c7d80b3dcb7458e370f4d9fe26ce72f2c
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-12-16 12:41:29 +0900

    add map and character

---
+++ src/bubble/test/ex02/Player.java
@@ -0,0 +1,29 @@
+package bubble.test.ex02;
+
+import javax.swing.*;
+
+public class Player extends JLabel {
+
+    private int x;
+    private int y;
+
+    private ImageIcon playerR, playerL;
+
+    public Player(){
+        initObject();
+        initSetting();
+    }
+
+    private void initObject(){
+        playerR = new ImageIcon("images/playerR.png");
+        playerL = new ImageIcon("images/playerL.png");
+    }
+    private void initSetting(){
+        x = 55;
+        y = 535;
+
+        setIcon(playerR);
+        setSize(50,50);
+        setLocation(x,y);
+    }
+}

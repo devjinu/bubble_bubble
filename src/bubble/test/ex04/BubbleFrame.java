070ff16f83c8c7e4137132251565c3e4ddf2559c
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-12-22 11:27:06 +0900

    add character left and right movement

---
+++ src/bubble/test/ex04/BubbleFrame.java
@@ -0,0 +1,82 @@
+package bubble.test.ex04;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+
+    private JLabel backgroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initObject();
+        initSetting();
+        initListner();
+        setVisible(true);
+    }
+
+    private void initObject() {
+        backgroundMap = new JLabel(new ImageIcon("images/backgroundMap.png"));
+        setContentPane(backgroundMap);
+
+        player = new Player();
+        add(player);
+
+    }
+
+    private void initSetting() {
+        setSize(1000, 640);
+        getContentPane().setLayout(null); // absolute layout(자유롭게 그림을 그릴 수 있다)
+
+        setLocationRelativeTo(null); //JFrame 가운데 배치
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌 때 JVM도 같이 종료
+    }
+
+    private void initListner() {
+        // 키보드 클릭 이벤트 핸들러
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // System.out.println(e.getKeyCode());
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        if (!player.isLeft()) {
+                            player.left();
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        if (!player.isRight()) {
+                            player.right();
+                        }
+                        break;
+                    case KeyEvent.VK_UP:
+                        player.up();
+                        break;
+                    // 아래 방향키로 캐릭터가 이동할 일은 없음
+                    /* case KeyEvent.VK_DOWN:
+                        player.down();
+                        break; */
+                }
+            }
+
+            // 키보드 릴리즈 이벤트 핸들러
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_LEFT:
+                        player.setLeft(false);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.setRight(false);
+                        break;
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+}

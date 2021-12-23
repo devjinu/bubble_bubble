f5387ff3cd746ae454f0cede6fa6ebaa7e601e64
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-12-23 10:53:58 +0900

    restrict a movement when approaching the wall

---
+++ src/bubble/test/ex07/BackgroundPlayerService.java
@@ -0,0 +1,52 @@
+package bubble.test.ex07;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+// 메인스레드 -> 키보드 이벤트를 처리
+// 백그라운드에서 계속 관찰 -> 새로운 스레드
+public class BackgroundPlayerService implements Runnable {
+
+    private BufferedImage image;
+    private Player player;
+
+    public BackgroundPlayerService(Player player) {
+        this.player = player;
+        try {
+            InputStream is;
+            image = ImageIO.read(new File("images/backgroundMapService.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // 색상 확인
+            Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
+            Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+
+            if (leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+                System.out.println("왼쪽 벽에 충돌");
+                player.setLeftWallCrash(true);
+                player.setLeft(false);
+            } else if (rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+                System.out.println("오른쪽 벽에 충돌");
+                player.setRightWallCrash(true);
+                player.setRight(false);
+            }else {
+               player.setLeftWallCrash(false);
+               player.setRightWallCrash(false);
+            }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/bubble/test/ex07/BubbleFrame.java
@@ -0,0 +1,84 @@
+package bubble.test.ex07;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+
+    private JLabel backgroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initObject();
+        initSetting();
+        initListner();
+        setVisible(true);
+    }
+
+    private void initObject() {
+        backgroundMap = new JLabel(new ImageIcon("images/backGroundMap.png"));
+        setContentPane(backgroundMap);
+
+        player = new Player();
+        add(player);
+
+    }
+
+    private void initSetting() {
+        setSize(1000, 640);
+        getContentPane().setLayout(null); // absolute layout(자유롭게 그림을 그릴 수 있다)
+
+        setLocationRelativeTo(null); //JFrame 가운데 배치
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌 때 JVM도 같이 종료
+    }
+
+    private void initListner() {
+        // 키보드 클릭 이벤트 핸들러
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // System.out.println(e.getKeyCode());
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        if (!player.isLeft() && !player.isLeftWallCrash()) {
+                            player.left();
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        if (!player.isRight() && !player.isRightWallCrash()) {
+                            player.right();
+                        }
+                        break;
+                    case KeyEvent.VK_UP:
+                        if (!player.isUp() && !player.isDown()) {
+                            player.up();
+                        }
+                        break;
+                    // 아래 방향키로 캐릭터가 이동할 일은 없음
+                    /* case KeyEvent.VK_DOWN:
+                        player.down();
+                        break; */
+                }
+            }
+
+            // 키보드 릴리즈 이벤트 핸들러
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        player.setLeft(false);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.setRight(false);
+                        break;
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+}

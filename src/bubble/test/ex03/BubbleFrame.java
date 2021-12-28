65b9808fec3c54bfec74af7e49e17e3d5ab87064
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-12-28 23:34:16 +0900

    commit

---
+++ src/bubble/test/ex03/BubbleFrame.java
@@ -0,0 +1,63 @@
+package bubble.test.ex03;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+
+    private JLabel backgroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initObject();
+        initSetting();
+        initListner();
+        setVisible(true);
+    }
+
+    private void initObject() {
+        backgroundMap = new JLabel(new ImageIcon("images/backgroundMap.png"));
+        setContentPane(backgroundMap);
+
+        player = new Player();
+        add(player);
+
+    }
+
+    private void initSetting() {
+        setSize(1000, 640);
+        getContentPane().setLayout(null); // absolute layout(자유롭게 그림을 그릴 수 있다)
+
+        setLocationRelativeTo(null); //JFrame 가운데 배치
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌 때 JVM도 같이 종료
+    }
+
+    private void initListner() {
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println(e.getKeyCode());
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        player.left();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.right();
+                        break;
+                    case KeyEvent.VK_UP:
+                        player.up();
+                        break;
+                    // 아래 방향키로 캐릭터가 이동할 일은 없음
+                    // case KeyEvent.VK_DOWN:
+                    // player.down();
+                    // break;
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+}

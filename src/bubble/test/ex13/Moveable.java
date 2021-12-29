ae817c227adc0e9df7232548fa07b4a05d4cd85c
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-12-29 17:51:14 +0900

    commit

---
+++ src/bubble/test/ex13/Moveable.java
@@ -0,0 +1,10 @@
+package bubble.test.ex13;
+
+public interface Moveable {
+    public abstract void left();
+    public abstract void right();
+    public abstract void up();
+    // 어댑터 패턴 x
+    // default를 사용하면 인터페이스도 몸체가 있는 메서드를 만들 수 있다(다중 상속x)
+    default public void down(){};
+}

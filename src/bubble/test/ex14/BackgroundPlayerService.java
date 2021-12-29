a259bcb6148f0157fca3f4ab1c8542fe691e995f
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-12-29 18:12:03 +0900

    code refactoring

---
+++ src/bubble/test/ex14/BackgroundPlayerService.java
@@ -0,0 +1,68 @@
+package bubble.test.ex14;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+// 메인스레드 -> 키보드 이벤트를 처리
+// 백그라운드에서 계속 관찰 -> 새로운 스레드
+public class BackgroundPlayerService implements Runnable {
+
+    private BufferedImage image;
+    private Player player;
+
+    public BackgroundPlayerService(Player player) {
+        this.player = player;
+        try {
+            InputStream is;
+            image = ImageIO.read(new File("images/backgroundMapService.png"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // 색상 확인
+            Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
+            Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+            // -2가 나오면 바닥이 흰색
+            int bottomColor = image.getRGB(player.getX() + 10, player.getY() + 50 + 5) // -> 왼쪽 바닥
+                    + image.getRGB(player.getX() + 40, player.getY() + 50 + 5); //-> 오른쪽 바닥
+
+
+            // 바닥 충돌 확인
+            if (bottomColor != -2) {
+                // System.out.println("bottomColor : "+ bottomColor);
+                // System.out.println("바닥에 있음");
+                player.setDown(false);
+            } else {
+                if (!player.isUp()&&!player.isDown()) {
+                    player.down();
+                }
+            }
+
+            // 외벽 충돌 확인
+            if (leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+                // System.out.println("왼쪽 벽에 충돌");
+                player.setLeftWallCrash(true);
+                player.setLeft(false);
+            } else if (rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+                // System.out.println("오른쪽 벽에 충돌");
+                player.setRightWallCrash(true);
+                player.setRight(false);
+            } else {
+                player.setLeftWallCrash(false);
+                player.setRightWallCrash(false);
+            }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/bubble/test/ex14/Bubble.java
@@ -0,0 +1,134 @@
+package bubble.test.ex14;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+
+@Getter
+@Setter
+public class Bubble extends JLabel implements Moveable {
+
+    // 의존성 컴포지션
+    private Player player;
+    private BackgroundBubbleService backgroundBubbleService;
+    private BubbleFrame mContext;
+
+    // 위치 상태
+    private int x;
+    private int y;
+
+    // 움직임 상태
+    private boolean left;
+    private boolean right;
+    private boolean up;
+
+    // 적을 맞춘 상태
+    private int state; // 0(물방울), 1(적을 가둔 물방울)
+
+    private ImageIcon bubble; // 일반 물방울
+    private ImageIcon bubbled; // 적을 가둔 물방울
+    private ImageIcon bomb; // 물방울이 터진 상태
+
+    public Bubble(BubbleFrame mContext) {
+        this.mContext = mContext;
+        this.player = mContext.getPlayer();
+        initObject();
+        initSetting();
+    }
+
+    private void initObject() {
+        bubble = new ImageIcon("images/bubble.png");
+        bubbled = new ImageIcon("images/bubbled.png");
+        bomb = new ImageIcon("images/bomb.png");
+
+        backgroundBubbleService = new BackgroundBubbleService(this);
+
+    }
+
+    private void initSetting() {
+        up = false;
+        left = false;
+        right = false;
+
+        this.x = player.getX();
+        this.y = player.getY();
+
+        setIcon(bubble);
+        setSize(50, 50);
+
+        state = 0;
+
+    }
+
+    @Override
+    public void left() {
+        left = true;
+        for (int i = 0; i < 400; i++) {
+            x--;
+            setLocation(x, y);
+            if (backgroundBubbleService.leftWall()) {
+                left = false;
+                break;
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        up();
+    }
+
+    @Override
+    public void right() {
+        right = true;
+        for (int i = 0; i < 400; i++) {
+            x++;
+            setLocation(x, y);
+            if (backgroundBubbleService.rightWall()) {
+                right = false;
+                break;
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        up();
+    }
+
+    @Override
+    public void up() {
+        up = true;
+        while (up) {
+            y--;
+            setLocation(x, y);
+            if (backgroundBubbleService.topWall()) {
+                up = false;
+                break;
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // 천장에 버블이 도착하고나서 3초 후에 메모리에서 소멸
+        clearBubble();
+    }
+
+    private void clearBubble() {
+        try {
+            Thread.sleep(3000);
+            setIcon(bomb);
+            Thread.sleep(500);
+            // BubbleFrame의 bubble이 메모리에서 소멸된다.
+            mContext.remove(this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
